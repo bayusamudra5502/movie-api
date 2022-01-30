91c3604233f15e7f92951cbac009814434ebfed6
bayusamudra5502 <contact@bayusamudra.my.id>
2022-01-30 18:12:45 +0700

    Menambahkan README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Movie API
+
+Repo ini merupakan API Movie menggunakan Typescript, Node.JS, Express, Postgresql, dan Docker
--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 mkdir -p private
 openssl rand -out ./private/auth.key -base64 1024
-# docker build --tag gdsc_movie_api:latest .
+
+docker build --tag gdsc_movie_api:latest .
