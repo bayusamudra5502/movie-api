c2051e111ff9e63a524addb7a710a6d6d17e540c
bayusamudra5502 <contact@bayusamudra.my.id>
2022-02-01 07:51:28 +0700

    Menambahkan api

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ openssl rand -out ./dist/private/auth.key -base64 1024
 mkdir -p ./dist/templates
 cp ./src/templates/* ./dist/templates
 
+mkdir -p ./dist/static
+cp ./public/* ./dist/static
+
 npm run build:style
 npm run build:script
 npm run migrate:prod
